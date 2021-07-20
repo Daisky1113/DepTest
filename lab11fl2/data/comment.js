93532d738347ccbf37a9a314544a0d2e5f14d4dc
daisky <daisky.shadow@gmail.com>
2021-07-20 09:45:26 +0900

    問題作成

---
+++ lab11fl2/data/comment.js
@@ -0,0 +1,20 @@
+const comments = [
+    {
+        id: 1,
+        title: 'hogeっていいですね',
+        body: 'hogeはああしてそうしてこうですね',
+        userName: 'hoge'
+    },
+    {
+        id: 2,
+        title: 'fooっていいですね',
+        body: 'fooはああしてそうしてこうですね',
+        userName: 'foo'
+    },
+    {
+        id: 3,
+        title: 'barっていいですね',
+        body: 'barはああしてそうしてこうですね',
+        userName: 'bar'
+    },
+]
